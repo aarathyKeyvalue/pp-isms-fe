dfe2c01267700052633f079d84fa5f05bbe5da56
aarathyKeyvalue <aarathy@keyvalue.systems>
2023-04-27 11:55:43 +0530

    Performance configuration webpack

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,11 @@ module.exports = {
     libraryTarget: 'umd',
     clean: true
   },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
   optimization: {
     minimize: true,
     minimizer: [
